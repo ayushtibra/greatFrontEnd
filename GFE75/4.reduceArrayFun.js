4e86e8c9c6b20b4cba89c474f50f51b0a12ee3d7
User <aayushtibra1997@gmail.com>
2025-09-08 17:56:59 +0530

    reduce func polyfill

---
+++ GFE75/4.reduceArrayFun.js
@@ -0,0 +1,29 @@
+Array.prototype.myReduce = function (callbackFn, thisArg) {
+    const noInitialValue = thisArg === undefined;
+  const len = this.length;
+
+  if (noInitialValue && len === 0) {
+    throw new TypeError('Reduce of empty array with no initial value');
+  }
+
+  let acc = noInitialValue ? this[0] : thisArg;
+  let startingIndex = noInitialValue ? 1 : 0;
+
+  // let result = initialValue;
+
+    for(let i = startingIndex; i<this.length; i++){
+        if(Object.hasOwn(this, i)){
+           acc = callbackFn.call(thisArg, acc, this[i], i, this);
+        }
+    }
+
+    return acc;
+};
+
+// Example usage
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.myReduce((acc, curr) => {
+    return acc + curr;
+}, 10);
+console.log(sum);
