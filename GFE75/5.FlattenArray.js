3ef5fbdbb3c89b24fd77337b024a1b14c2863ea0
User <aayushtibra1997@gmail.com>
2025-09-10 19:38:55 +0530

    flatten array custom

---
+++ GFE75/5.FlattenArray.js
@@ -0,0 +1,46 @@
+const FlattenArray = (arr) => {
+    const result = arr.reduce((acc, curr) => {
+        if(Array.isArray(curr)){
+            acc.push(...FlattenArray(curr));
+        } else {
+            acc.push(curr);
+        }
+        return acc;
+    },[])
+
+    return result;
+}
+
+// or ------------------------------------------------------------------------------------------------
+
+// const FlattenArray = (arr) => {
+//     return arr.reduce(
+//         (acc, curr) => acc.concat(
+//           Array.isArray(curr) ?
+//           FlattenArray(curr) : curr),
+//         [],
+//       );
+// }
+
+// or ------------------------------------------------------------------------------------------------
+
+// function flattenArray(arr) {
+//     const result = [];
+//     arr.forEach(item => {
+//         if (Array.isArray(item)) {
+//             result.push(...flattenArray(item)); // recursively flatten inner arrays
+//         } else {
+//             result.push(item);
+//         }
+//     });
+//     return result;
+// }
+
+// const nestedArray = [1, 2, [3, 4], [5, [6, 7]]];
+// const flattened = flattenArray(nestedArray);
+// console.log(flattened); // [1, 2, 3, 4, 5, 6, 7]
+
+
+const arr = [1,2,3,4, [5,6,7,8, [9,10,[11],12]]];
+const result = FlattenArray(arr);
+console.log(result); 
