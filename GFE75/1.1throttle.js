640ab519d0092fbe6dcda087f691dda7e4698065
User <aayushtibra1997@gmail.com>
2025-09-10 20:01:14 +0530

    throttle custom func

---
+++ GFE75/1.1throttle.js
@@ -0,0 +1,30 @@
+const throttle = (callback, delay) => {
+    let shouldThrottle = false;
+
+    return function (...args) {
+        if (shouldThrottle) {
+        return;
+        }
+
+        shouldThrottle = true;
+        setTimeout(function () {
+            shouldThrottle = false;
+        }, delay);
+
+        callback.apply(this, args);
+    };
+
+}
+
+// Some examples of where throttle is used:
+// Resize events
+// API calls during window resizing
+// Button spam protection
+// Autocomplete suggestions
+
+// how to use throttle in scroll event
+window.addEventListener('scroll', throttle(function () {
+    console.log('scroll');
+}, 1000));
+
+// https://css-tricks.com/debouncing-throttling-explained-examples/
